e11936208ae0145c1d1136351d294e0b6a2436b5
tetsuyaf1100 <tetsuyaf1100@gmail.com>
2021-11-29 11:22:56 +0000

    Done tutorial #7

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# ListViewとDetailViewを取り込み
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Post
+
+class Index(ListView):
+    model = Post
+
+class Detail(DetailView):
+    model = Post
+
+# CreateViewは新規作成画面を簡単に作るためのView
+class Create(CreateView):
+    model = Post
+    fields = ["title", "body", "category", "tags"]
+
+# 編集対象にするフィールド
+class Update(UpdateView):
+    model = Post
+    fields = ["title", "body", "category", "tags"]
+
+class Delete(DeleteView):
+    model = Post
+    success_url = "/"
